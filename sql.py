4ee22018c6330e8f051690d4d59b872f8d095ec1
Yingqi Wang <ireneyingqi@gmail.com>
2022-04-15 15:57:43 +1000

    database add friend function update

--- sql.py
+++ sql.py
@@ -14,7 +14,7 @@ class SQLDatabase():
     '''
 
     # Get the database running
-    def __init__(self, database_arg):
+    def __init__(self, database_arg='database.db'):
         self.conn = sqlite3.connect(database_arg) # create a connection object, creat a database
         # the cursor object allow us to excute the commands and queries in the database
         self.cur = self.conn.cursor()
@@ -52,12 +52,14 @@ class SQLDatabase():
             Id INT,
             username TEXT,
             password TEXT,
+            pk TEXT,
+            friendlist TEXT,
             admin INTEGER DEFAULT 0
         );""")
 
         self.commit()
         # Add our admin user
-        self.add_user('admin', admin_password, admin=1)
+        self.add_user('admin', admin_password, None, admin=1)
 
     #-----------------------------------------------------------------------------#
     #-----------------------------------------------------------------------------#
@@ -68,22 +70,23 @@ class SQLDatabase():
     #-----------------------------------------------------------------------------#
 
     # Add a user to the database
-    def add_user(self,username, password, admin=0):
+    def add_user(self,username, password, pk, admin=0):
         sql_cmd = """
                 INSERT INTO Users
-                (Id,username,password,admin)
-                VALUES({id}, '{username}', '{password}', {admin})
+                (Id,username,password,pk,admin)
+                VALUES({id}, '{username}', '{password}', '{pk}',  {admin})
             """
 
-        sql_cmd = sql_cmd.format(id=0,username=username, password=password, admin=admin)
+        sql_cmd = sql_cmd.format(id=0,username=username, password=password, pk=pk, admin=admin)
 
         self.execute(sql_cmd)
         self.commit()
 
         return True
-
+    
+    #--------------------------------------------------------------------------#
     #-----------------------------------------------------------------------------
-
+    #--------------------------------------------------------------------------#
     # Check login credentials
     def check_credentials(self, username, password):
         sql_query = """
@@ -91,10 +94,10 @@ class SQLDatabase():
                 FROM Users
                 WHERE username = '{username}' AND password = '{password}'
             """
-
         sql_query = sql_query.format(username=username, password=password)
         
         self.execute(sql_query)
+        self.commit()
         
         # If our query returns
         if self.cur.fetchone():
@@ -102,27 +105,56 @@ class SQLDatabase():
         else:
             return False
         
+    #--------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------#
+    # check does username already exist or not  
     def check_username_exist(self, username):
         sql_query = """
                 SELECT 1 
                 FROM Users
                 WHERE username = '{username}'
             """
-
         sql_query = sql_query.format(username=username)
         
         self.execute(sql_query)
+        self.commit()
         
         if self.cur.fetchone():
             # username exist in database
             return True
         else:
             return False
-        
+    
+    #--------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------#   
     def add_friend(self,user_id, friend_id):
         # check friend exist in the database
         # if exist, get the public key 
-        return 
+         
+        # get the friend list
+        sql_query = """
+                SELECT friendlist 
+                FROM Users
+                WHERE username = '{username}'
+            """
+            
+        sql_query = sql_query.format(username=user_id)
+        self.execute(sql_query)
+        self.commit()
+        
+        if self.cur.fetchone():
+            friendlis = self.cur.fetchone()
+            if friendlis == None:
+                friendlis
+            msg = 'user exist'
+            return msg
+        else:
+            # user not found/ user_id not exist in the database
+            msg = "User_id not correct"
+            return msg        
+        
         
     def get_friendlist(self, username):
         
@@ -131,9 +163,9 @@ class SQLDatabase():
 """
 # create our database
 database = SQLDatabase() 
-database.database_setup(admin_password='admin')
-print(database.add_user('irene', 'abc', admin=0))
+database.database_setup('admin')
+print(database.add_user('irene', 'abc',None,admin=0))
 print(database.check_credentials('irene','abc'))
 print(database.check_credentials('kkk', '123'))
-
+print(database.add_friend('irene', 'kk'))
 """
